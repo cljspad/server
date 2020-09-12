9074262a0a32aaa59fd185d15372c0aba5ccf1ec
Thomas Crowley <git@tscrowley.dev>
2020-09-12 16:15:43 +1000

    Initial commit of server

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject cljsfiddle/server "1.0.0"
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring "1.8.1"]
+                 [org.clojure/tools.namespace "1.0.0"]
+                 [ring-cors "0.1.13"]]
+  :main cljsfiddle.server)
---
+++ src/cljsfiddle/server.clj
@@ -0,0 +1,67 @@
+(ns cljsfiddle.server
+  (:require [clojure.tools.namespace.find :as ns.find]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.edn :as edn]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.cors :refer [wrap-cors]])
+  (:import (java.util.jar JarFile)))
+
+(def sandbox
+  {"1" (JarFile. (io/file "/Users/thomascrowley/Code/clojure/sandbox/target/sandbox-1.0.0-standalone.jar"))})
+
+(defn sym->classpath [sym]
+  (-> sym
+      name
+      (str/replace "." "/")
+      (str/replace "-" "_")))
+
+(defn try-read-cache [^JarFile sandbox name]
+  (when-let [entry (or (.getEntry sandbox (format "cljsfiddle/%s.cljs.cache.json" (sym->classpath name)))
+                       (.getEntry sandbox (format "cljsfiddle/%s.cljc.cache.json" (sym->classpath name))))]
+    (with-open [stream (.getInputStream sandbox entry)]
+      (slurp stream))))
+
+(defn load-source
+  [version {:keys [name macros]}]
+  (when-let [sandbox ^JarFile (get sandbox version)]
+    (let [entries (set (ns.find/sources-in-jar sandbox (if macros ns.find/clj ns.find/cljs)))
+          entry   (sym->classpath name)]
+      (when-let [entry (if macros
+                         (or (entries (str entry ".clj"))
+                             (entries (str entry ".cljc")))
+                         (or (entries (str entry ".cljs"))
+                             (entries (str entry ".cljc"))))]
+        (with-open [stream (.getInputStream sandbox (.getEntry sandbox entry))]
+          {:source (slurp stream)
+           :lang   :clj
+           :cache  (when-not macros
+                     (try-read-cache sandbox name))})))))
+
+(defmulti rpc :request)
+
+(defmethod rpc :default [_]
+  {:status 404})
+
+(defmethod rpc :env/load [{:keys [sandbox/version opts]}]
+  (if-let [resp (load-source version opts)]
+    {:status 200
+     :body (pr-str resp)
+     :headers {"Content-Type" "application/edn"}}
+    {:status 404}))
+
+(defn handler
+  [req]
+  (if (= :post (:request-method req))
+    (rpc (-> req :body slurp edn/read-string))
+    {:status 405}))
+
+(defn -main [& _]
+  (jetty/run-jetty
+   (wrap-cors handler
+              :access-control-allow-origin [#".*"]
+              :access-control-allow-methods [:get :post])
+   {:port 3000 :join? false}))
+
+(comment
+ (load-source "1" {:name 'reagent.debug :macros true}))
